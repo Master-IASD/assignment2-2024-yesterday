ea503c7dcf3f1e47596b8a870895cca81805a29c
hangyue zhao <103986258+zhysalari@users.noreply.github.com>
2024-10-19 16:32:57 +0200

    hard truncation

--- generate.py
+++ generate.py
@@ -6,15 +6,22 @@ import argparse
 
 from model import Generator
 from utils import load_model
+def truncated_normal(shape, mean=0.0, std=1.0, truncation=2.0):
 
+    z = torch.randn(shape).cuda() * std + mean
+    
+    while True:
+        mask = (z < -truncation) | (z > truncation)
+        if not mask.any():
+            break
+        z[mask] = torch.randn(mask.sum()).cuda() * std + mean
+    
+    return z
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate Normalizing Flow.')
     parser.add_argument("--batch_size", type=int, default=2048,
                       help="The batch size to use for training.")
     args = parser.parse_args()
-
-
-
 
     print('Model Loading...')
     # Model Pipeline
@@ -35,7 +42,8 @@ if __name__ == '__main__':
     n_samples = 0
     with torch.no_grad():
         while n_samples<10000:
-            z = torch.randn(args.batch_size, 100).cuda()
+            # z = torch.randn(args.batch_size, 100).cuda()
+            z =truncated_normal((args.batch_size, 100), truncation=2.0).cuda()
             x = model(z)
             x = x.reshape(args.batch_size, 28, 28)
             for k in range(x.shape[0]):
