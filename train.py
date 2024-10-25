7032be45d1b192e0ef1033053cbecfeaa27a8e6c
MariemAa <m.aalabou@aui.ma>
2024-10-25 11:32:15 +0200

    Intial Commit

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import torch 
+import torch
 import os
 from tqdm import trange
 import argparse
@@ -11,72 +11,74 @@ from model import Generator, Discriminator
 from utils import D_train, G_train, save_models
 
 
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
-    parser.add_argument("--epochs", type=int, default=100,
-                        help="Number of epochs for training.")
-    parser.add_argument("--lr", type=float, default=0.0002,
-                      help="The learning rate to use for training.")
-    parser.add_argument("--batch_size", type=int, default=64, 
-                        help="Size of mini-batches for SGD")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train Normalizing Flow.")
+    parser.add_argument(
+        "--epochs", type=int, default=100, help="Number of epochs for training."
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.0002,
+        help="The learning rate to use for training.",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=64, help="Size of mini-batches for SGD"
+    )
 
     args = parser.parse_args()
 
-
-    os.makedirs('chekpoints', exist_ok=True)
-    os.makedirs('data', exist_ok=True)
+    os.makedirs("chekpoints", exist_ok=True)
+    os.makedirs("data", exist_ok=True)
 
     # Data Pipeline
-    print('Dataset loading...')
+    print("Dataset loading...")
     # MNIST Dataset
-    transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize(mean=(0.5), std=(0.5))])
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(mean=(0.5), std=(0.5))]
+    )
 
-    train_dataset = datasets.MNIST(root='data/MNIST/', train=True, transform=transform, download=True)
-    test_dataset = datasets.MNIST(root='data/MNIST/', train=False, transform=transform, download=False)
+    train_dataset = datasets.MNIST(
+        root="data/MNIST/", train=True, transform=transform, download=True
+    )
+    test_dataset = datasets.MNIST(
+        root="data/MNIST/", train=False, transform=transform, download=False
+    )
 
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=args.batch_size, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=args.batch_size, shuffle=False
+    )
+    print("Dataset Loaded.")
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                               batch_size=args.batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                              batch_size=args.batch_size, shuffle=False)
-    print('Dataset Loaded.')
-
-
-    print('Model Loading...')
+    print("Model Loading...")
     mnist_dim = 784
-    G = torch.nn.DataParallel(Generator(g_output_dim = mnist_dim)).cuda()
+    G = torch.nn.DataParallel(Generator(g_output_dim=mnist_dim)).cuda()
     D = torch.nn.DataParallel(Discriminator(mnist_dim)).cuda()
 
-
     # model = DataParallel(model).cuda()
-    print('Model loaded.')
-    # Optimizer 
-
-
+    print("Model loaded.")
+    # Optimizer
 
     # define loss
-    criterion = nn.BCELoss() 
+    criterion = nn.BCELoss()
 
     # define optimizers
-    G_optimizer = optim.Adam(G.parameters(), lr = args.lr)
-    D_optimizer = optim.Adam(D.parameters(), lr = args.lr)
+    G_optimizer = optim.Adam(G.parameters(), lr=args.lr)
+    D_optimizer = optim.Adam(D.parameters(), lr=args.lr)
 
-    print('Start Training :')
-    
+    print("Start Training :")
+
     n_epoch = args.epochs
-    for epoch in trange(1, n_epoch+1, leave=True):           
+    for epoch in trange(1, n_epoch + 1, leave=True):
         for batch_idx, (x, _) in enumerate(train_loader):
             x = x.view(-1, mnist_dim)
             D_train(x, G, D, D_optimizer, criterion)
             G_train(x, G, D, G_optimizer, criterion)
 
         if epoch % 10 == 0:
-            save_models(G, D, 'checkpoints')
-                
-    print('Training done')
+            save_models(G, D, "checkpoints")
 
-        
+    print("Training done")
