5e28e1ddfc658358922e9c61f968c0159d6b835b
Xichen <108240415+SeasonXC@users.noreply.github.com>
2024-10-29 15:20:18 +0000

    generating test

--- generate.py
+++ generate.py
@@ -6,6 +6,8 @@ import argparse
 
 from model import Generator
 from utils import load_model
+
+
 def truncated_normal(shape, mean=0.0, std=1.0, truncation=2.0):
 
     z = torch.randn(shape).cuda() * std + mean
@@ -27,23 +29,26 @@ if __name__ == '__main__':
     # Model Pipeline
     mnist_dim = 784
 
-    model = Generator(g_output_dim = mnist_dim).cuda()
-    model = load_model(model, 'checkpoints')
+    # model = Generator(g_output_dim = mnist_dim).cuda()
+    # model = load_model(model, 'checkpoints')
+    # model = torch.nn.DataParallel(model).cuda()
+    # model.eval()
+    
+    # Load G_Vanilla
+    model = torch.load('checkpoints/G_Vanilla.pth').cuda()
     model = torch.nn.DataParallel(model).cuda()
     model.eval()
 
     print('Model loaded.')
-
-
-
+    
     print('Start Generating')
     os.makedirs('samples', exist_ok=True)
 
     n_samples = 0
     with torch.no_grad():
         while n_samples<10000:
-            # z = torch.randn(args.batch_size, 100).cuda()
-            z =truncated_normal((args.batch_size, 100), truncation=2.0).cuda()
+            z = torch.randn(args.batch_size, 100).cuda()
+            # z =truncated_normal((args.batch_size, 100), truncation=2.0).cuda()
             x = model(z)
             x = x.reshape(args.batch_size, 28, 28)
             for k in range(x.shape[0]):
